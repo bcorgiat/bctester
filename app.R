a34cbf3430c793a50a246fc25aee96017309a047
bcorgiat <81588330+bcorgiat@users.noreply.github.com>
2025-01-16 14:06:10 -0800

    a

--- app.R
+++ app.R
@@ -1,4 +1,4 @@
-options(repos = c(CRAN = "https://cran.rstudio.com"))
+options(repos = c(CRAN = "https://cran.rstudio.com"), timeout = 400)
 
 if (!requireNamespace("remotes", quietly = TRUE)) {
   install.packages("remotes")
@@ -6,7 +6,7 @@ if (!requireNamespace("remotes", quietly = TRUE)) {
 
 # Install 'bctester' from GitHub if not already installed
 if (!requireNamespace("bctester", quietly = TRUE)) {
-  remotes::install_github("bcorgiat/bctester", auth_token = Sys.getenv("GITHUB_PAT"))
+  remotes::install_github("bcorgiat/bctester", auth_token = NULL)
 }
 
 library(bctester)
