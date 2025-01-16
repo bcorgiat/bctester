40fbda76291581ddf0cc1a09beb402a7f6c4271e
bcorgiat <81588330+bcorgiat@users.noreply.github.com>
2025-01-16 11:09:36 -0800

    a

--- app.R
+++ app.R
@@ -1,2 +1,11 @@
+if (!requireNamespace("remotes", quietly = TRUE)) {
+  install.packages("remotes")
+}
+
+# Install 'bctester' from GitHub if not already installed
+if (!requireNamespace("bctester", quietly = TRUE)) {
+  remotes::install_github("bcorgiat/bctester")
+}
+
 library(bctester)
 bctester::run_app()
